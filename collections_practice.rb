ecf56573064c20f31c48609ef4fce6ef77f40f7d
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-23 22:33:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(number)
+  
+  number.sort do|a,b|
+  a <=> b 
+  end 
+end 
 
+def sort_array_desc(number)
+   
+  number.sort do|a,b|
+  b <=> a 
+  end 
+  
+end 
+
+def sort_array_char_count(string)
+  string.sort do |a,b|
+    a.length <=> b.length  
+     
+  end
+end 
+
+def swap_elements(array)
+  
+ # arr = array.join(" ")
+  array.sort do|a,b|
+  a[1], b[2] = a[2], b[1] 
+ end 
+end 
+
+def reverse_array(number)
+  number.reverse 
+
+end 
+
+def kesha_maker(string)
+
+end 
+
+def find_a
+  
+end 
+
+def sum_array
+  
+end 
+  
+  
